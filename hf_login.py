f20d78de916a25f988fa077df16c751da5a6b4a0
violetdenim <kristinazipa@yandex.ru>
2024-09-16 16:48:12 +0300

    small comment

--- hf_login.py
+++ hf_login.py
@@ -3,4 +3,5 @@ from huggingface_hub._login import _login
 
 dotenv.load_dotenv()
 token = os.environ.get("HF_AUTH")
+# login to hf hub with valid credentials
 _login(token=token, add_to_git_credential=True)
