6eef8153640f0fd401fe24f80ed5e32fc6f09657
violetdenim <kristinazipa@yandex.ru>
2024-09-19 15:32:52 +0300

    [+] socket\queue time out

--- audio-tcp/client.py
+++ audio-tcp/client.py
@@ -1,58 +1,62 @@
 # Welcome to PyShine
 # This is client code to receive video and audio frames over TCP
 
-import socket,os
-import threading, wave, pyaudio, pickle,struct
-import dotenv
+import os
+import socket, time
+import threading, pyaudio, pickle, struct
 
+import dotenv
 dotenv.load_dotenv()
 
-# host_name = socket.gethostname()
-host_ip = os.environ.get("HOST_IP")
+# host_ip = os.environ.get("HOST_IP")
+host_name = socket.gethostname()
+host_ip = socket.gethostbyname(host_name)#os.environ.get("HOST_IP")
 port = int(os.environ.get("HOST_PORT"))
 
-def audio_stream():
-	p = pyaudio.PyAudio()
-	CHUNK = 1024
-	stream = p.open(format=p.get_format_from_width(2),
-					channels=2,
-					rate=44100,
-					output=True,
-					frames_per_buffer=CHUNK)
-					
-	# create socket
-	client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	socket_address = (host_ip,port-1)
-	print('server listening at',socket_address)
-	client_socket.connect(socket_address) 
-	print("CLIENT CONNECTED TO",socket_address)
-	data = b""
-	payload_size = struct.calcsize("Q")
-	while True:
-		try:
-			while len(data) < payload_size:
-				packet = client_socket.recv(4*1024) # 4K
-				if not packet: break
-				data+=packet
-			packed_msg_size = data[:payload_size]
-			data = data[payload_size:]
-			msg_size = struct.unpack("Q",packed_msg_size)[0]
-			while len(data) < msg_size:
-				data += client_socket.recv(4*1024)
-			frame_data = data[:msg_size]
-			data  = data[msg_size:]
-			frame = pickle.loads(frame_data)
-			stream.write(frame)
+class Reciever(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		# os.environ['SDL_AUDIODRIVER'] = 'dsp'
+		self.p = pyaudio.PyAudio()
+		self.CHUNK = 1024
+		self.waiting_period = 3.0
 
-		except:
-			
-			break
+	def run(self):
+		stream = self.p.open(format=self.p.get_format_from_width(2),
+						channels=2, rate=44100, output=True, frames_per_buffer=self.CHUNK)
+		block_size = 4 * self.CHUNK
+		# create socket
+		client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket_address = (host_ip, port-1)
+		client_socket.connect(socket_address)
+		data = b""
+		payload_size = struct.calcsize("Q")
+		while True:
+			try:
+				while len(data) < payload_size:
+					client_socket.settimeout(self.waiting_period)
+					packet = client_socket.recv(block_size) # 4K
+					data += packet
+				packed_msg_size = data[:payload_size]
+				data = data[payload_size:]
+				msg_size = struct.unpack("Q",packed_msg_size)[0]
+				while len(data) < msg_size:
+					client_socket.settimeout(self.waiting_period)
+					data += client_socket.recv(block_size)
+				frame_data, data = data[:msg_size], data[msg_size:] # pass left-over to the next message
+				frame = pickle.loads(frame_data)
+				stream.write(frame)
+				# data = b""
+				# wait for the next data
+			except Exception as e:
+				print(f"No transmission for {self.waiting_period} seconds! Closing stream. {e}")
+				stream.close()
+				print("Exiting!")
+				self.p.terminate()
+				client_socket.close()
+				os._exit(0)
 
-	client_socket.close()
-	print('Audio closed')
-	os._exit(1)
-	
-t1 = threading.Thread(target=audio_stream, args=())
-t1.start()
+if __name__ == "__main__":
+	Reciever().start()
 
-
+ 
--- audio-tcp/server.py
+++ audio-tcp/server.py
@@ -1,48 +1,79 @@
 # This is server code to send video and audio frames over TCP
-
 import socket, os
-import threading, wave, pyaudio, pickle, struct
-
+import threading, wave, pickle, struct
+from queue import Queue
 import dotenv
+import time, random
 
 dotenv.load_dotenv()
-
-# host_name = socket.gethostname()
-host_ip = os.environ.get("HOST_IP")
+host_name = socket.gethostname()
+host_ip = socket.gethostbyname(host_name)#os.environ.get("HOST_IP")
+print(host_name, host_ip)
 port = int(os.environ.get("HOST_PORT"))
 
-def audio_stream():
-    server_socket = socket.socket()
-    server_socket.bind((host_ip, (port-1)))
+class QueuedThread(threading.Thread):
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
 
-    server_socket.listen(5)
-    CHUNK = 1024
-    wf = wave.open("temp.wav", 'rb')
+        self.server_socket = socket.socket()
+        self.server_socket.bind((host_ip, (port-1)))
+        self.server_socket.listen(5)
+        self.chunk = 1024
     
-    p = pyaudio.PyAudio()
-    print('server listening at',(host_ip, (port-1)))
-   
-    
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    input=True,
-                    frames_per_buffer=CHUNK)
-
-             
-
-    client_socket, addr = server_socket.accept()
- 
-    data = None
-    while True:
-        if client_socket:
+    def run(self):
+        client_socket, addr = self.server_socket.accept()
+        try:
             while True:
-              
-                data = wf.readframes(CHUNK)
-                a = pickle.dumps(data)
-                message = struct.pack("Q",len(a))+a
-                client_socket.sendall(message)
-                
-t1 = threading.Thread(target=audio_stream, args=())
-t1.start()
+                # close connection after 10 seconds of delay
+                file_name = self.queue.get(timeout=10.0) 
+                # prepare data to stream
+                wf = wave.open(file_name, 'rb')
+                while True:
+                    try:
+                        data = wf.readframes(self.chunk)
+                        if len(data) == 0:
+                            print(f"Finished transmission for file {file_name}")
+                            self.queue.task_done()
+                            break # break inner while, move to the next file
+                        a = pickle.dumps(data)
+                        # a = str.encode(file_name)
+                        message = struct.pack("Q", len(a))+a
+                        client_socket.sendall(message)        
+                    except:
+                        print(f"Transmission interrupted by client for file {file_name}. Exception: {e}")
+                        self.server_socket.close()
+                        self.queue.task_done()
+                        return -1
+        except Exception as e:
+            print(f"Empty queue for 10.0 seconds! {e}")
+            self.server_socket.close()
+            # self.queue.task_done()
+            return -1
+            
+            
+
+class TaskGenerator(threading.Thread):
+    def __init__(self, queue, list_of_files):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.list_of_files = list_of_files
+    
+    def run(self):
+        for file in self.list_of_files:
+            # push with random delay
+            time.sleep(random.random() * 3.0)
+            self.queue.put(file)
+
+def queued_audio_server(files=["short.wav", "temp.wav", "short.wav", "temp.wav", "short.wav"]):
+    queue = Queue()
+    generator = TaskGenerator(queue, [os.path.abspath(f) for f in files])
+    server = QueuedThread(queue)
+    server.start()
+    generator.start()
+    queue.join()
+
+if __name__ == "__main__":
+    queued_audio_server(["short.wav", "short.wav"])
+
 
