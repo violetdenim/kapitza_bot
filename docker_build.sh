dd85804cdade8d29712fe25781adc9c339737794
violetdenim <kristinazipa@yandex.ru>
2024-09-25 14:09:28 +0300

    [~] docker_buil.sh param tag

--- docker_build.sh
+++ docker_build.sh
@@ -1,11 +1,15 @@
 # build and push docker containers
-docker build -t kszipa/kapitza -f docker/Dockerfile.base .
-docker build -t kszipa/kapitza-bot -f docker/Dockerfile.bot .
-docker build -t kszipa/kapitza-server -f docker/Dockerfile.server .
-docker build -t kszipa/kapitza-client -f docker/Dockerfile.client .
+# read first argument from command line. It specifies TAG
+TAG=$1
+echo $TAG
+docker build -t kszipa/kapitza:$TAG -f docker/Dockerfile.base .
+docker build -t kszipa/kapitza-bot:$TAG -f docker/Dockerfile.bot .
+docker build -t kszipa/kapitza-server:$TAG -f docker/Dockerfile.server .
+docker build -t kszipa/kapitza-client:$TAG -f docker/Dockerfile.client .
 
 # push them to hub
-docker push kszipa/kapitza
-docker push kszipa/kapitza-bot
-docker push kszipa/kapitza-server
-docker push kszipa/kapitza-client
+docker push kszipa/kapitza:$TAG
+docker push kszipa/kapitza-bot:$TAG
+docker push kszipa/kapitza-server:$TAG
+docker push kszipa/kapitza-client:$TAG
+
