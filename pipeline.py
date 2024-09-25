64f7b5a6718b60c9372d29474ab5d5d34ce0bfe0
violetdenim <kristinazipa@yandex.ru>
2024-09-25 14:17:57 +0300

    [~] parameter use_llama_guard moved to Pipeline

--- pipeline.py
+++ pipeline.py
@@ -13,12 +13,15 @@ dotenv.load_dotenv()
 
 class Pipeline:
     def __init__(self,
-                 model_url=f"https://huggingface.co/QuantFactory/Meta-Llama-3-8B-Instruct-GGUF/resolve/main/Meta-Llama-3-8B-Instruct.Q4_K_M.gguf?download=true"):
+                 model_url=f"https://huggingface.co/QuantFactory/Meta-Llama-3-8B-Instruct-GGUF/resolve/main/Meta-Llama-3-8B-Instruct.Q4_K_M.gguf?download=true",
+                 use_llama_guard=False):
         # model_url = "https://huggingface.co/QuantFactory/Meta-Llama-3.1-8B-GGUF/resolve/main/Meta-Llama-3.1-8B.Q4_K_M.gguf?download=true"
         hf_token = os.environ.get('HF_AUTH')
         self.asr = ASRProcessor(hf_token=hf_token)
         self.llm = LLMProcessor(os.environ.get(
-            "PROMPT_PATH"), os.environ.get("RAG_PATH"), model_url=model_url)
+            "PROMPT_PATH"), os.environ.get("RAG_PATH"),
+            model_url=model_url,
+            use_llama_guard=use_llama_guard)
         self.tts = TTSProcessor(os.environ.get(
             "AUDIO_PATH"), hf_token=hf_token)
 
@@ -48,12 +51,11 @@ if __name__ == '__main__':
     # print(f"Answer to Василий saved to {result}")
     
     msg = "Здравствуйте, Сергей Петрович!"
-    
     # override system outputs
     default_out, default_err = sys.stdout, sys.stderr
     with open(os.devnull, 'w'), open(os.devnull, 'w') as sys.stdout, sys.stderr:
         sys.stderr = open(os.devnull, 'w')
-        pipe = Pipeline()
+        pipe = Pipeline(use_llama_guard=False)
         result = pipe.process(user_name="Василий", user_message=msg, output_mode="text")
     sys.stdout, sys.stderr = default_out, default_err
     print(f"Василий: {msg}")
