e3566f04f78e3f53b2ad6af80dadb624b5039401
violetdenim <kristinazipa@yandex.ru>
2024-11-06 12:33:49 +0300

    question mark fails in windows FS

--- local_folder_service.py
+++ local_folder_service.py
@@ -100,8 +100,8 @@ if __name__ == "__main__":
     
     input_folder = ".received"
     output_folder = ".generated"
-    # model_url = "https://huggingface.co/QuantFactory/Meta-Llama-3-8B-Instruct-GGUF/resolve/main/Meta-Llama-3-8B-Instruct.Q4_K_M.gguf?download=true"
-    model_url=f"https://huggingface.co/kzipa/kap34_8_8_10/resolve/main/kap34_8_8_10.Q4_K_M.gguf?download=true"
+    # model_url = "https://huggingface.co/QuantFactory/Meta-Llama-3-8B-Instruct-GGUF/resolve/main/Meta-Llama-3-8B-Instruct.Q4_K_M.gguf"
+    model_url=f"https://huggingface.co/kzipa/kap34_8_8_10/resolve/main/kap34_8_8_10.Q4_K_M.gguf"
     use_llama_guard = False
 
     os.makedirs(input_folder, 0o777, True)
