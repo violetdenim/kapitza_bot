a55468f0913a7194b145350a9dadc3b50e147453
violetdenim <kristinazipa@yandex.ru>
2024-11-14 16:49:51 +0300

    proper naming set

--- connection/runme.bat
+++ connection/runme.bat
@@ -1,2 +1,2 @@
-start python ftpc.py 10.16.84.41 3033 send
-start python ftps.py 3033 recv
+start python ftpc.py 10.16.84.41 3033 .received
+start python ftps.py 3033 .generated
--- connection/runme.sh
+++ connection/runme.sh
@@ -1,2 +1,2 @@
-python ftps.py 3033 recv
-python ftpc.py 10.16.65.11 3033 send
+gnome-terminal "python ftps.py 3033 ../.received " &
+gnome-terminal "python ftpc.py 10.16.65.11 3033 ../.generated" &
