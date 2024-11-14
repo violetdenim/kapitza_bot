5eaa546c4362949134faf3f814655e29ac33bbfb
violetdenim <kristinazipa@yandex.ru>
2024-11-14 15:16:02 +0300

    TTS as service

--- src/tts.py
+++ src/tts.py
@@ -71,12 +71,17 @@ class TTSThread(threading.Thread):
         self.input = input
         self.engine = TTSProcessor(**params)
         self.output = output
+        self._kill = threading.Event()
 
     def run(self):
         while True:
+            if self._kill.wait(0.05):
+                print("Killing TTSThread: kill signal")
+                return
             package = self.input.get(block = True)
             if isinstance(package, int) and package == 9: # kill
-                self.input.all_tasks_done()
+                self.input.task_done()
+                print("Killing TTSThread: end of queue")
                 return
 
             assert(isinstance(package, tuple) or isinstance(package, list))
@@ -94,3 +99,86 @@ class TTSThread(threading.Thread):
             if self.output:
                 self.output.put(result)
             self.input.task_done()
+            
+    def kill(self):
+        self._kill.set()
+            
+
+def _split_text(text, min_length=128):
+    import re
+    import dotenv
+    dotenv.load_dotenv('../.env')
+    sentence_list = [s.strip(' ') for s in re.split("(\.|!|\?)", text) if len(s)]
+    sentence_list = [sentence_list[i] + sentence_list[i + 1] for i in range(0, len(sentence_list), 2)]
+    
+    audio_block = ""
+    for sentence in sentence_list:
+        if len(audio_block) + len(sentence) < min_length: 
+            audio_block += " " + sentence
+        else:
+            if len(audio_block):
+                yield audio_block
+            audio_block = sentence
+    if len(audio_block):
+        yield audio_block
+    return
+    
+# function to monitor 
+def _do_folder_monitoring(input_folder=".received", output_folder='.generated'):
+    for filename in [input_folder, output_folder]:
+        if not os.path.exists(filename):
+            os.makedirs(filename, 0o777, True)
+    import time
+    input_queue = Queue()
+    
+    my_thread = TTSThread(input_queue, None, checkpoint_path=os.environ.get("AUDIO_PATH"), output_dir=output_folder)
+    my_thread.start()
+    
+    while True:
+        time.sleep(0.1)
+        files = os.listdir(input_folder)
+        # fetch files using creation date
+        if len(files):
+            sorted_files = list(sorted([os.path.join(input_folder, file_name) for file_name in files], key=lambda x: os.path.getctime(x)))
+            for input_file_name in sorted_files:
+                if not os.path.exists(input_file_name):
+                    continue
+                name = os.path.splitext(os.path.split(input_file_name)[-1])[0]
+                with open(input_file_name, 'r', encoding='utf-16') as f:
+                    user_text = f.read()
+                    for i, sentence in enumerate(_split_text(user_text, min_length=128)):
+                        input_queue.put([sentence, ".wav", f"{name}_{i}.wav"])
+                os.remove(input_file_name)
+
+def _demo_generation():
+    text = """Hello Science News Explores readers! I am a digital clone of Sergey Kapiitsa. He was a famous scientist who passed away in two thousand twelve. Now, artificial intelligence has made it possible to mimic his voice and likeness."""
+    input_queue = Queue()
+    for i, sentence in enumerate(_split_text(text, min_length=128)):
+        input_queue.put([sentence, ".wav", f"test_{i}.wav"])
+    input_queue.put(9)
+    
+    my_thread = TTSThread(input_queue, None, checkpoint_path=os.environ.get("AUDIO_PATH"))
+    my_thread.start()
+    input_queue.join()
+    # my_thread.kill()
+    
+def _push_files_to_folder(folder=".received"):
+    os.makedirs(folder, 0o777, True)
+    text = """Hello Science News Explores readers! I am a digital clone of Sergey Kapiitsa.He was a famous scientist who passed away in two thousand twelve. Now, artificial intelligence has made it possible to mimic his voice and likeness."""
+    with open(os.path.join(folder, "test.txt"), "w+", encoding='utf-16') as f:
+        f.write(text)
+    
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        mode = int(sys.argv[1])
+    else:
+        mode = 0
+    match mode:
+        case 0: _do_folder_monitoring()
+        case 1: _push_files_to_folder()
+        case 2: _demo_generation()
+    
+    
+    
+    
