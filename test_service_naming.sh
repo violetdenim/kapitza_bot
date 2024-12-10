18c0a407eafa8496a717c803e7330f54f7d72c6d
violetdenim <kristinazipa@yandex.ru>
2024-12-10 12:43:57 +0300

    test fix

--- test_service_naming.sh
+++ test_service_naming.sh
@@ -4,7 +4,4 @@ cp files/name1.wav .received
 sleep 3
 cp files/test.wav .received
 sleep 3
-cp files/color.wav .received
-sleep 3
-cp files/музыка.txt .received
-sleep 3
+cp files/color.txt .received
